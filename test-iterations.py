dd58bbc8438fdf29620c1e771a2081cd00707e98
Soren Atmakuri Davidsen <soren@atmakuridavidsen.com>
2014-10-14 13:49:12 +0530

    ok

--- test-iterations.py
+++ test-iterations.py
@@ -8,26 +8,8 @@ import numpy as np
 import fylearn.fuzzylogic as fl
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.cross_validation import cross_val_score
+from test import execute_one
 import sklearn.tree as tree
-
-def execute_one(L, X, y):
-
-    # iterate learners
-    output = []
-    for l in L:
-        # cross validation
-        scores = paper.cross_val_score(l, X, y, cv=10)
-
-        output.append("$%.2f \pm %.2f$" % (np.mean(scores), np.std(scores)))
-        print "---"
-        print "dataset", dataset
-        print "learner", l
-
-        print "scores %s, mean %f, std %f" % \
-            (str(scores), np.mean(scores), np.std(scores))
-
-    return output
-
 
 logger = logging.getLogger("fpcga")
 logger.setLevel(logging.INFO)
@@ -44,10 +26,11 @@ L = (
     fpcga.FuzzyPatternClassifierGA(iterations=500), # all
     fpcga.FuzzyPatternClassifierGA(iterations=1000), # all
 )
-            
+
 # iterate over datsets
 import paper
 dataset = ("Telugu Vowels", "vowel.csv")
 X, y = paper.load(paper.path(dataset))
 output = execute_one(L, X, y)
-print "%s & %s \\\\" % (dataset[0], " & ".join(output))
+
+print ",".join(dataset) + "," + ",".join(map(str, output))
